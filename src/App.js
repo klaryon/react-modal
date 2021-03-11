9da3c43ab3348524c464084d2b0a6daf78b60d0f
klaryon <klary.ardon@gmail.com>
2021-03-11 21:25:44 +0100

    button created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const Button = styled.button`
+    min-width: 100px;
+    padding: 16px 32px;
+    border-radius: 4px;
+    border: none;
+    background: #141414;
+    color: #fff;
+    font-size: 24px;
+    cursor: pointer;
+`;
 
 function App() {
   return (
     <>
-      <h1>What's up</h1>
+      <Container>
+        <Button>I'm a modal</Button>
+      </Container>
     </>
   );
 }
