9e22d5df1b88facc3f75841ead30c9cc7333921d
klaryon <klary.ardon@gmail.com>
2021-03-12 09:57:52 +0100

    animation added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useRef, useEffect, useCallback} from 'react'
+import {useSpring, animated} from 'react-spring'
 import styled from 'styled-components';
 import { MdClose } from 'react-icons/md';
 
@@ -64,19 +65,31 @@ const CloseModalButton =  styled(MdClose)`
 `;
 
 export const Modal = ({showModal, setShowModal}) => {
+    const modalRef = useRef()
+
+    const animation = useSpring({
+        config: {
+            duration: 250
+        },
+        opacity: showModal ? 1 : 0,
+        transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+    })
+
     return (
         <>
             {showModal ? (
                 <Background>
-                    <ModalWrapper showModal>
-                        <ModalImg src={require('./modal.jpg').default} alt='camera' />
-                        <ModalContent>
-                            <h1>Are you ready?</h1>
-                            <p>Get exclusive access to our next launch</p>
-                            <button>Join now</button>
-                        </ModalContent>
-                        <CloseModalButton aria-label='Close modal' onClick={() => setShowModal(prev => !prev)} />
-                    </ModalWrapper>
+                    <animated.div style={animation}>
+                        <ModalWrapper showModal>
+                            <ModalImg src={require('./modal.jpg').default} alt='camera' />
+                            <ModalContent>
+                                <h1>Are you ready?</h1>
+                                <p>Get exclusive access to our next launch</p>
+                                <button>Join now</button>
+                            </ModalContent>
+                            <CloseModalButton aria-label='Close modal' onClick={() => setShowModal(prev => !prev)} />
+                        </ModalWrapper>
+                    </animated.div>
                 </Background>
             ) : null}
         </>
