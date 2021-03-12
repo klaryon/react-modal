0232bcb5599b0381559d42aab983193365e32f9d
klaryon <klary.ardon@gmail.com>
2021-03-12 09:47:22 +0100

    modal image added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+import { Modal } from './components/Modal';
+import { GlobalStyle } from './globalStyles';
 
 const Container = styled.div`
     display: flex;
@@ -20,10 +22,18 @@ const Button = styled.button`
 `;
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(prev => !prev)
+  }
+
   return (
     <>
       <Container>
-        <Button>I'm a modal</Button>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
       </Container>
     </>
   );
